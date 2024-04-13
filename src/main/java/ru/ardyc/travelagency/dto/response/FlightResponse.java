297b200bbd5c6207683ff2bb8ffe550849c2b822
ArhostCode <e-gulyakin@mail.ru>
2024-04-13 14:36:44 +0300

    fix(api): Fix flight contract

--- src/main/java/ru/ardyc/travelagency/dto/response/FlightResponse.java
+++ src/main/java/ru/ardyc/travelagency/dto/response/FlightResponse.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 public record FlightResponse(
         UUID id,
+        String from,
+        String to,
         OffsetDateTime startDate,
         OffsetDateTime finishDate,
         double price
